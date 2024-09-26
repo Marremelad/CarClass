6bded5d2d06b4fe014f4f6094c9d17ac6e3d569f
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-09-26 15:35:54 +0200

    Change called methods

--- Program.cs
+++ Program.cs
@@ -6,8 +6,6 @@ class Program
     {
         Car car = new Car("Hyundai", "i30n", 2019, 0.0);
         car.Drive(1000);
-        car.DisplayMileage();
-        car.Drive(1000);
-        car.DisplayMileage();
+        car.DisplayCar();
     }
 }
