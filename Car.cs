f44b24d9744d45a6bf0553bb2b7fb51e9f845162
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-09-26 15:30:01 +0200

    Commit repository

---
+++ Car.cs
@@ -0,0 +1,46 @@
+﻿namespace CarClass;
+
+public class Car
+{
+    private string _make;
+    private string _model;
+    private int _year;
+    private double _mileage;
+
+    public Car(string make, string model, int year, double mileage)
+    {
+        this._make = make;
+        this._model = model;
+        this._year = year;
+        this._mileage = mileage;
+    }
+
+    public double Mileage
+    {
+        get => _mileage;
+        set
+        {
+            if (_mileage >= 2000)
+            {
+                throw new ArgumentException("Your car has passed the recommended mileage.");
+            }
+
+            _mileage = value;
+        }
+    }
+
+    public void Drive(double kilometers)
+    {
+        Mileage += (kilometers / 10);
+    }
+
+    public void DisplayMileage()
+    {
+        Console.WriteLine(_mileage);
+    }
+
+    public void DisplayCar()
+    {
+        Console.WriteLine($"Make: {_make}\nModel: {_model}\nYear: {_year}\nMileage: {_mileage}");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace CarClass;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car car = new Car("Hyundai", "i30n", 2019, 0.0);
+        car.Drive(1000);
+        car.DisplayMileage();
+        car.Drive(1000);
+        car.DisplayMileage();
+    }
+}
